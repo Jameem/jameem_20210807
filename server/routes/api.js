0cfbf57b6500eeecce6936e00b161f98c98d3892
Jameem <jameemkurikkal.mp@gmail.com>
2021-08-07 11:32:57 +0530

    fix: cleanup

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,6 @@
 var express = require("express")
 var router = express.Router()
 
-// const entityController = require("./controllers/entityController")
-
 router.get("/", (req, res) =>
   res.status(200).send({
     message: "Welcome to the Case Study API v1.0",
