dbf96786b36590f35baa6e528b5cb659795630ec
Jameem <jameemkurikkal.mp@gmail.com>
2021-08-07 18:07:58 +0530

    feat: add video category controller

---
+++ server/controllers/videoCategory.js
@@ -0,0 +1,33 @@
+const Sequelize = require("sequelize")
+
+const db = require("../db/models")
+
+// Get list of all categories
+exports.listForSelect = async (req, res, next) => {
+  try {
+    const categories = await db.VideoCategory.findAll({
+      attributes: [
+        [Sequelize.literal('"VideoCategory"."id"'), "value"],
+        [Sequelize.literal('"VideoCategory"."name"'), "label"],
+      ],
+      order: [["id", "ASC"]],
+    }).catch((e) => {
+      throw e
+    })
+
+    return res.status(200).send({
+      success: true,
+      message: "Categories fetched successfully.",
+      data: categories,
+    })
+  } catch (error) {
+    let message = "Uh-oh, Something went wrong!"
+
+    console.error(error)
+    return res.status(400).send({
+      error: error && error.message ? error.message : error,
+      success: false,
+      message,
+    })
+  }
+}
